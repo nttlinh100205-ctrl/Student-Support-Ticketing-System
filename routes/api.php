3811cf995f509c657caec9b284d18e014b1086af
Nguyễn Thanh Ngân <2311060747@hunre.edu.vn>
2026-09-25 13:54:09 +0700

    chi giu lai folder report-service

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-use App\Http\Controllers\Api\ReportController;
-
-Route::middleware('can-view-reports')->group(function () {
-    Route::get('/reports/statistics', [ReportController::class, 'statistics']);
-    Route::get('/reports/export', [ReportController::class, 'export']);
-});
